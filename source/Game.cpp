ebdd88259e8a2befeb65186fdbf572497c6788eb
Saadiqjan <saadiq.shah@hotmail.com>
2026-07-22 10:33:35 -0400

    refactored code, moved game loop out of main

---
+++ source/Game.cpp
@@ -0,0 +1,43 @@
+/* Author: Saadiq Shahsamand
+ * Creation Date: Jul 19, 2026
+ * Modified Date: Jul 21, 2026
+ * Filename: Game.cpp
+ * Project Name: HappyHorses
+ * Description: Game loop will be handled here
+ */
+
+#include "Game.h"
+
+#include <nds.h>
+
+#include "GraphicsManager.h"
+
+void Game::init()
+{
+	powerOn(POWER_ALL_2D);
+	
+	lcdMainOnBottom();
+	
+	graphics.initVideo();
+	graphics.initBackgrounds();
+
+	graphics.loadTitleScreen();
+}
+
+void Game::run()
+{
+	while (true)
+	{
+		swiWaitForVBlank();
+	}
+}
+
+void Game::update()
+{
+	
+}
+
+void Game::draw()
+{
+	
+}
---
+++ source/GraphicsManager.cpp
@@ -0,0 +1,112 @@
+/* Author: Saadiq Shahsamand
+ * Creation Date: Jul 21, 2026
+ * Modified Date: Jul 21, 2026
+ * Filename: GraphicsManager.h
+ * Project Name: HappyHorses
+ * Description: Manages setting up and loading graphics
+ */
+
+#include "GraphicsManager.h"
+
+#include <nds.h>
+
+#include "titleScreen.h"
+#include "titleScreenSub.h"
+
+void GraphicsManager::initVideo()
+{
+    // Allocate VRAM
+    vramSetPrimaryBanks(
+    	VRAM_A_MAIN_BG_0x06000000,
+    	VRAM_B_MAIN_BG_0x06020000,
+    	VRAM_C_SUB_BG_0x06200000,
+    	VRAM_D_LCD
+	);
+
+	// Allocate VRAM bank E for sprites
+	vramSetBankE(VRAM_E_MAIN_SPRITE);
+
+    // Main screen (top)
+    videoSetMode(
+        MODE_5_2D |
+        DISPLAY_BG3_ACTIVE |
+        DISPLAY_SPR_ACTIVE |
+        DISPLAY_SPR_1D
+    );
+
+    // Sub screen (bottom)
+    videoSetModeSub(
+        MODE_5_2D |
+        DISPLAY_BG3_ACTIVE |
+        DISPLAY_SPR_ACTIVE |
+        DISPLAY_SPR_1D
+    );
+}
+
+void GraphicsManager::initBackgrounds()
+{
+    // Top screen background (BG3)
+    REG_BG3CNT = BG_BMP8_256x256 |
+                 BG_BMP_BASE(0) |
+                 BG_PRIORITY(3);
+
+    // Identity transformation matrix
+    REG_BG3PA = 1 << 8;
+    REG_BG3PB = 0;
+    REG_BG3PC = 0;
+    REG_BG3PD = 1 << 8;
+
+    // Position at top-left
+    REG_BG3X = 0;
+    REG_BG3Y = 0;
+
+
+    // Bottom screen background (BG3)
+    REG_BG3CNT_SUB = BG_BMP8_256x256 |
+                     BG_BMP_BASE(0) |
+                     BG_PRIORITY(3);
+
+    // Identity transformation matrix
+    REG_BG3PA_SUB = 1 << 8;
+    REG_BG3PB_SUB = 0;
+    REG_BG3PC_SUB = 0;
+    REG_BG3PD_SUB = 1 << 8;
+
+    // Position at top-left
+    REG_BG3X_SUB = 0;
+    REG_BG3Y_SUB = 0;
+}
+
+void::GraphicsManager::loadTitleScreen()
+{
+    // Copy palette for main BG
+    dmaCopy(
+        titleScreenPal,
+        BG_PALETTE,
+        titleScreenPalLen
+    );
+
+    // Top screen
+    dmaCopyHalfWords(
+        DMA_CHANNEL,
+        titleScreenBitmap,
+        (uint16*)BG_BMP_RAM(0),
+        titleScreenBitmapLen
+    );
+
+
+    // Copy palette for sub BG
+    dmaCopy(
+        titleScreenSubPal,
+        BG_PALETTE_SUB,
+        titleScreenSubPalLen
+    );
+
+    // Bottom screen
+    dmaCopyHalfWords(
+        DMA_CHANNEL,
+        titleScreenSubBitmap,
+        (uint16*)BG_BMP_RAM_SUB(0),
+        titleScreenSubBitmapLen
+    );
+}
