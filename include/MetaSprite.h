fd3482532d95c14d36720afd527091973a005296
Saadiqjan <saadiq.shah@hotmail.com>
2026-08-06 21:30:07 -0400

    Added metasprite class

---
+++ include/MetaSprite.h
@@ -0,0 +1,34 @@
+/* Author: Saadiq Shahsamand
+ * Creation Date: Jul 31, 2026
+ * Modified Date: Jul 31, 2026
+ * Filename: MetaSprite.h
+ * Project Name: HappyHorses
+ * Description: Header file for meta sprite
+ */
+
+#pragma once
+#include "Sprite.h"
+#include "OamManager.h"
+#include <vector>
+
+using namespace std;
+
+struct SpritePart
+{
+    Sprite sprite;
+    int offsetX, offsetY;
+};
+
+class MetaSprite {
+    public:
+        bool addPart(OamManager& mgr, const SpriteConfig& cfg, int offsetX, int offsetY);
+
+        void setPosition(int x, int y);
+        void setFrame(int frame);
+        void setVisible(bool visible);
+        void setFlip(bool h, bool v);
+        void draw();
+    private:
+        vector<SpritePart> parts;
+        int posX = 0, posY = 0;
+};
---
+++ source/MetaSprite.cpp
@@ -0,0 +1,62 @@
+/* Author: Saadiq Shahsamand
+ * Creation Date: Jul 31, 2026
+ * Modified Date: Aug 1, 2026
+ * Filename: MetaSprite.cpp
+ * Project Name: HappyHorses
+ * Description: store a larger sprite made up of smaller sprites
+ */
+
+#include "MetaSprite.h"
+#include "Sprite.h"
+#include <nds.h>
+
+using namespace std;
+
+bool MetaSprite::addPart(OamManager& mgr, const SpriteConfig& cfgIn, int offsetX, int offsetY)
+{
+    SpritePart part;
+    part.offsetX = offsetX;
+    part.offsetY = offsetY;
+
+    SpriteConfig cfg = cfgIn;
+    cfg.oam = mgr.getOam();
+    cfg.spriteId = mgr.allocate();
+    if (cfg.spriteId == -1) return false;
+
+    part.sprite.init(cfg);
+    parts.push_back(std::move(part));
+    return true;
+}
+
+void MetaSprite::setPosition(int x, int y)
+{
+    posX = x; posY = y;
+    for (auto& part : parts)
+    {
+        part.sprite.setPosition(posX + part.offsetX, posY + part.offsetY);
+    }
+}
+
+void MetaSprite::setFrame(int frame)
+{
+    for (auto& part : parts) 
+    {
+        part.sprite.setFrame(frame);
+    }
+}
+
+void MetaSprite::setVisible(bool visible)
+{
+    for (auto& part : parts) 
+    {
+        part.sprite.setVisible(visible);
+    }
+}
+
+void MetaSprite::draw()
+{
+    for (auto& part : parts) 
+    {
+        part.sprite.draw();
+    }
+}
