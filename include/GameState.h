046dfc69eab5430c143f07f2065bc7bbe71fbff8
Saadiqjan <saadiq.shah@hotmail.com>
2026-08-12 19:20:02 -0400

    Added enum class to keep track of game state

---
+++ include/GameState.h
@@ -0,0 +1,18 @@
+/* Author: Saadiq Shahsamand
+ * Creation Date: Aug 12, 2026
+ * Modified Date: Aug 12, 2026
+ * Filename: GameState.h
+ * Project Name: HappyHorses
+ * Description: for keeping track of game state
+ */
+
+#pragma once
+#include <cstdint>
+
+enum class GameStateType : uint8_t
+{
+    TitleScreen
+    SelectSave,
+    Gameplay,
+    PauseScreen
+};
