17871d692ed7dde4b37ca0ae7a9811d50831afb9
Saadiqjan <saadiq.shah@hotmail.com>
2026-08-19 17:57:12 -0400

    Rewrote buttons

--- include/Button.h
+++ include/Button.h
@@ -1,6 +1,6 @@
 /* Author: Saadiq Shahsamand
  * Creation Date: Jul 22, 2026
- * Modified Date: Jul 22, 2026
+ * Modified Date: Aug 19, 2026
  * Filename: Button.h
  * Project Name: HappyHorses
  * Description: Header file for button class
@@ -8,28 +8,46 @@
 
 #pragma once
 
+#include <nds.h>
+#include <string>
+#include <variant>
+#include <type_traits>
+
+#include "TextLayer.h"
+#include "SpriteManager.h"
 #include "Sprite.h"
+#include "MetaSprite.h"
+
+using namespace std;
 
 class Button {
     public:
-        Button(int x, int y, int width, int height);
+        Button(SpriteManager& spriteMgr, TextLayer& textLayer);
+    
+        bool createSingle(const SpriteConfig& cfg, int width, int height);
 
-        bool contains();
+        bool createMulti();
+        bool addPart(const SpriteConfig& cfg, int offsetX, int offsetY);
+        void finalizeMulti(int width, int height);
 
         void setPosition(int x, int y);
+        void setLabel(const std::string& text);
+        void setPressed(bool pressed);
 
-        int getX() const;
-        int getY() const;
-        int getWidth() const;
-        int getHeight() const;
+        void draw();
+
+        bool contains(int touchX, int touchY) const;
+        bool isLoaded() const { return !std::holds_alternative<std::monostate>(renderable); }
     private:
-        int x;
-        int y;
+        SpriteManager& spriteMgr;
+        TextLayer& textLayer;
 
-        int width;
-        int height;
+        int x = 0;
+        int y = 0;
+        int width = 0;
+        int height = 0;
 
-        Sprite leftSprite;
-        Sprite middleSprite;
-        Sprite rightSprite;
+        using Renderable = variant<std::monostate, Sprite*, MetaSprite*>;
+        Renderable renderable;
+        string label;
 };
--- source/Button.cpp
+++ source/Button.cpp
@@ -1,26 +1,84 @@
 /* Author: Saadiq Shahsamand
  * Creation Date: Jul 22, 2026
- * Modified Date: Jul 22, 2026
+ * Modified Date: Aug 19, 2026
  * Filename: Button.cpp
  * Project Name: HappyHorses
  * Description: Button class
  */
 
 #include "Button.h"
-#include "Sprite.h"
-#include <nds.h>
 
-Button::Button(int x, int y, int width, int height)
+Button::Button(SpriteManager& spriteMgr, TextLayer& textLayer)
+    : spriteMgr(spriteMgr), textLayer(textLayer)
 {
-    // leftSprite = new Sprite();
-    // middleSprite = new Sprite();
-    // rightSprite = new Sprite();
+}
+
+bool Button::createSingle(const SpriteConfig& cfg, int w, int h)
+{
+    Sprite* s = spriteMgr.createSprite(cfg);
+    if (!s) return false;
+
+    renderable = s;
+    width = w;
+    height = h;
+    return true;
+}
+
+void Button::createMulti()
+{
+    renderable = spriteMgr.createMetaSprite();
+}
+
+bool Button::addPart(const SpriteConfig& cfg, int offsetX, int offsetY)
+{
+    if (auto* m = std::get_if<MetaSprite*>(&renderable))
+        return (*m)->addPart(spriteMgr.oamManager(), cfg, offsetX, offsetY);
+    return false; // createMulti() wasn't called first
+}
+
+void Button::finalizeMulti(int w, int h)
+{
+    width = w;
+    height = h;
 }
 
 void Button::setPosition(int x, int y)
 {
-    this->x = x;
-    this->y = y;
+    posX = x;
+    posY = y;
 
-    //sprite.setPosition(x, y);
+    std::visit([x, y](auto&& r)
+    {
+        using T = std::decay_t<decltype(r)>;
+        if constexpr (std::is_same_v<T, Sprite*> || std::is_same_v<T, MetaSprite*>)
+        {
+            if (r) r->setPosition(x, y);
+        }
+    }, renderable);
+}
+
+void Button::setPressed(bool pressed)
+{
+    int frame = pressed ? 1 : 0;
+
+    std::visit([frame](auto&& r)
+    {
+        using T = std::decay_t<decltype(r)>;
+        if constexpr (std::is_same_v<T, Sprite*> || std::is_same_v<T, MetaSprite*>)
+        {
+            if (r) r->setFrame(frame);
+        }
+    }, renderable);
+}
+
+void Button::draw()
+{
+    if (!label.empty())
+        textLayer.drawTextCentered(posX, posY, width, height, label);
+}
+
+bool Button::contains(int touchX, int touchY) const
+{
+    return touchX >= posX && touchX < posX + width &&
+           touchY >= posY && touchY < posY + height;
 }
