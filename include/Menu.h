e3fd8558f5fcbaa51e21a2bbb789d57cfaf71073
Saadiqjan <saadiq.shah@hotmail.com>
2026-08-20 13:15:55 -0400

    Added menu class

---
+++ include/Menu.h
@@ -0,0 +1,37 @@
+/* Author: Saadiq Shahsamand
+ * Creation Date: Aug 19, 2026
+ * Modified Date: Aug 20, 2026
+ * Filename: Menu.h
+ * Project Name: HappyHorses
+ * Description: header file for menu
+ */
+
+#pragma once
+
+#include <nds.h>
+#include <vector>
+
+#include "Button.h"
+
+using namespace std;
+
+class Menu {
+    public:
+        Menu(SpriteManager& spriteMgr, TextLayer& textLayer);
+
+        Button& addButton();
+
+        void update(int touchX, int touchY, bool touching, bool justReleased);
+        void draw();
+
+        int selectedIndex() const { return lastSelected; }
+        Button* selectedButton() const;
+        
+        void clear();
+    private:
+        SpriteManager& spriteMgr;
+        TextLayer& textLayer;
+
+        vector<unique_ptr<Button>> buttons;
+        int lastSelected = -1;
+};
---
+++ source/Menu.cpp
@@ -0,0 +1,52 @@
+/* Author: Saadiq Shahsamand
+ * Creation Date: Aug 19, 2026
+ * Modified Date: Aug 20, 2026
+ * Filename: Menu.cpp
+ * Project Name: HappyHorses
+ * Description: Menu to keep track of menu objects
+ */
+
+#include "Menu.h"
+
+Menu::Menu(SpriteManager& mgr, TextLayer& layer)
+    : spriteMgr(mgr), textLayer(layer)
+{
+}
+
+Button& Menu::addButton()
+{
+    buttons.push_back(std::make_unique<Button>(spriteMgr, textLayer));
+    return *buttons.back();
+}
+
+void Menu::update(int touchX, int touchY, bool touching, bool justReleased)
+{
+    lastSelected = -1;
+    for (size_t i = 0; i < buttons.size(); ++i)
+    {
+        if (!buttons[i]->isLoaded()) continue;
+
+        bool over = buttons[i]->contains(touchX, touchY);
+        buttons[i]->setPressed(over && touching);
+
+        if (over && justReleased)
+            lastSelected = static_cast<int>(i);
+    }
+}
+
+void Menu::draw()
+{
+    for (auto& b : buttons)
+        if (b->isLoaded()) b->draw();
+}
+
+Button* Menu::selectedButton() const
+{
+    if (lastSelected < 0 || lastSelected >= (int)buttons.size()) return nullptr;
+    return buttons[lastSelected].get();
+}
+
+void Menu::clear()
+{
+    buttons.clear();
+}
