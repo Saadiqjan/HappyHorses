6c06525e1fcd27dde3698e4054ca0e0586a1e981
Saadiqjan <saadiq.shah@hotmail.com>
2026-08-25 19:16:51 -0400

    Added menu state

---
+++ include/state/MenuState.h
@@ -0,0 +1,29 @@
+/* Author: Saadiq Shahsamand
+ * Creation Date: Aug 25, 2026
+ * Modified Date: Aug 25, 2026
+ * Filename: MenuState.h
+ * Project Name: HappyHorses
+ * Description: header for menustate
+ */
+
+#pragma once
+
+#include "State.h"
+
+class MenuState {
+    public:
+        MenuState(std::vector<MenuEntry> entries,
+                  SpriteManager* spriteMgr,
+                  TextLayer* textLayer,
+                  GraphicsManager* gfxMgr);
+
+        void onEnter() override;
+        void onExit() override;
+        void update(u32 keysHeld, u32 keysDown, u32 keysUp) override;
+    private:
+        std::vector<MenuEntry> entries;
+        Menu menu;
+        SpriteManager* spriteMgr;
+        TextLayer* textLayer;
+        GraphicsManager* gfxMgr;
+};
