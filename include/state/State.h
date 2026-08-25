e2e4d393c44854a2b03c63a34ca20b7a3bbf2efd
Saadiqjan <saadiq.shah@hotmail.com>
2026-08-25 18:47:22 -0400

    Added class to act as blueprint of interface for game states

---
+++ include/state/State.h
@@ -0,0 +1,25 @@
+/* Author: Saadiq Shahsamand
+ * Creation Date: Aug 25, 2026
+ * Modified Date: Aug 25, 2026
+ * Filename: State.h
+ * Project Name: HappyHorses
+ * Description: for keeping parent class of state objects
+ */
+
+#pragma once
+
+#include <nds.h>
+
+class State {
+    public:
+        virtual ~State() = default;
+
+        virtual void onEnter() {}
+        virtual void onExit() {}
+        virtual void onPause() {}
+        virtual void onResume() {}
+
+        virtual void update(u32 keysHeld, u32 keysDown, u32 keysUp) = 0;
+        virtual void renderMain() {}
+        virtual void renderSub() {}
+};
